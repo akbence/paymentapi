5e7e696a21156437fda696a538571135596c243a
akben <akbence007@gmail.com>
2025-03-19 22:44:04 +0100

    documentation, validation added docs at: http://localhost:8080/swagger-ui/index.html

--- src/main/java/hu/payment/paymentapi/controller/PaymentController.java
+++ src/main/java/hu/payment/paymentapi/controller/PaymentController.java
@@ -2,7 +2,14 @@ package hu.payment.paymentapi.controller;
 
 import hu.payment.paymentapi.dto.SendMoneyRequest;
 import hu.payment.paymentapi.service.PaymentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/payment")
 @RequiredArgsConstructor
+@Tag(name = "Payment API", description = "Endpoints for managing payments")
+@Validated
 public class PaymentController {
 
     private final PaymentService paymentService;
 
+    @Operation(summary = "Transfer money from one to another", description = "Transfers a sum of money from a user to another." +
+            "If successful, sends a notification through Kafka.")
+    @ApiResponse(responseCode = "201", description = "Payment transferred successfully")
     @PostMapping("/sendMoney")
-    public void postSendMoney(@RequestBody SendMoneyRequest sendMoneyRequest){
+    public ResponseEntity<String> postSendMoney(@Valid @RequestBody SendMoneyRequest sendMoneyRequest){
         paymentService.sendMoney(sendMoneyRequest);
+        return new ResponseEntity<>("Transaction created successfully", HttpStatus.CREATED);
     }
 
 }
--- src/main/java/hu/payment/paymentapi/dto/SendMoneyRequest.java
+++ src/main/java/hu/payment/paymentapi/dto/SendMoneyRequest.java
@@ -1,6 +1,10 @@
 package hu.payment.paymentapi.dto;
 
 import hu.payment.paymentapi.model.Currency;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -8,9 +12,20 @@ import java.math.BigDecimal;
 @Data
 public class SendMoneyRequest {
 
+    @NotNull
     private String sender;
+
+    @NotNull
     private String recipient;
+
+    @NotNull
+    @DecimalMin(value = "0.01", message = "Amount must be greater than 0")
     private BigDecimal amount;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
     private Currency currency;
+
+    @NotNull
     private String transactionId;
 }
