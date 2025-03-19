99c4a4f64a138b0aace3eeced3e9e39d6c875764
akben <akbence007@gmail.com>
2025-03-19 11:57:47 +0100

    init

---
+++ src/test/java/hu/payment/paymentapi/InstantPaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.payment.paymentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantPaymentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
